c75b4467c9fde00b6b8e6aa774cabf91c7344167
sidikalamini <33785823+sidikalamini@users.noreply.github.com>
2022-02-22 12:09:34 +0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,30 @@
+import board
+import busio
+import digitalio
+import time
+import supervisor
+#Set UART Pin
+uart = busio.UART(board.D0, board.D1, baudrate=9600)
+get_input = True
+message_started = False
+message_print = []
+allstring = ""
+printshow = False
+while True:
+    if supervisor.runtime.serial_bytes_available:
+        allstring=""
+        userinput = input().strip() #input command
+        b = bytes(userinput, 'utf-8')
+        uart.write(b)
+        continue
+    byte_read = uart.readline()# read up to 32 bytes
+    if byte_read != None:
+        allstring += byte_read.decode()
+        printshow = True
+    else:
+        if printshow == True:
+            if allstring != "":
+                print(allstring)
+            allstring=""
+            printshow ==False
+
